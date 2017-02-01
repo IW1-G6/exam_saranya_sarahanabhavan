ae562961db38b071224040d7644371cb4a9e2cce
Shashouww <s.saranya93420@gmail.com>
2017-02-01 16:38:17 +0100

    Ajout de la classe stringFormatterTest.php

---
+++ tests/Lib/stringFormatterTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace tests\Lib;
+
+use Lib\stringFormatter;
+
+class stringFormatterTest extends \PHPUnit_Framework_TestCase
+{
+	public function testSuffix()
+    {
+        $exmFormat = new stringFormatter();
+
+        $dbExam = $exmFormat->suffix("toto", "tata");
+
+        $this->assertSame($dbExam, "tatatoto"); 
+
+       
+    }
+
+       public function testPrefix()
+    {
+        $exmFormat = new stringFormatter();
+
+        $dbExam = $exmFormat->prefix("toto", "tata");
+
+        $this->assertSame($dbExam, "tototata"); 
+    }
+
+     public function testToCamelCase()
+    {
+        $exmFormat = new stringFormatter();
+
+        $dbExam = $exmFormat->toCamelCase("toto", "tata");
+
+        $this->assertSame($dbExam, "totoTata"); 
+    }
+
+    public function concatString()
+    {
+        $exmFormat = new stringFormatter();
+
+        $dbExam = $exmFormat->toCamelCase("toto", "tata");
+
+        $this->assertSame($dbExam, "tototata"); 
+    }
+
+}
